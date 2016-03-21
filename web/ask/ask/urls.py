817ea74956a1f72664dd4fa7dc51b62ca3a9bd37
Ruben <rhatutyunov@mail.ru>
2016-03-21 17:35:26 +0000

    MVC #1

---
+++ web/ask/ask/urls.py
@@ -0,0 +1,19 @@
+from django.conf.urls import patterns, include, url
+from qa.views import test
+from django.contrib import admin
+admin.autodiscover()
+
+urlpatterns = patterns('',
+    # Examples:
+    # url(r'^$', 'ask.views.home', name='home'),
+    # url(r'^blog/', include('blog.urls')),
+
+    url(r'^admin/', include(admin.site.urls)),
+    url(r'^$', test),
+    url(r'^login/', test),
+    url(r'^signup/', test),
+    url(r'^question/(?P<id>\d+)/', test),
+    url(r'^ask/', test),
+    url(r'^popular/', test),
+    url(r'^new/', test)
+)
